c3e9258bebbd5d057671a0a6b8fdcf55fd19932b
veslie losius <veslielosius@yahoo.com>
2019-02-03 01:00:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
@@ -23,14 +23,14 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index_number()
+  
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+  
 end
 
 def retrieve_last_element_from_array(array)
-  array[-1]
+  
   
 end
